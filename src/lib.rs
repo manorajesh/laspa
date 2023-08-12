878be41ea0f51f648b4f4fe9e176bdc683e225bb
manorajesh <mano@raje.sh>
2023-08-12 00:41:34 -0700

    Added documentation to library for publishing

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,62 @@
-#![warn(missing_docs)]
+// #![warn(missing_docs)]
+
+/*!
+# läspa
+A simple programming language written in Rust. It's a Lisp-like language, but with a more
+traditional syntax. Each element of the language is designed to be as easy to implement as
+possible (hence the [RPN](https://en.wikipedia.org/wiki/Reverse_Polish_notation)), so that it
+can be used as a learning tool for people who want to learn how to write their own programming
+language. The language itself is not very useful, but it's a good starting
+point for learning how to write a programming language.
+
+## Example
+```ignore
+let x 10;
+while < x 100
+    := x + x 1;
+end
+
+if < x 100
+    return 1;
+else
+    return 2;
+end
+```
+
+## Syntax
+The syntax is very simple. Each statement is separated by a newline or a semicolon. Comments are
+denoted by `//`. The language is **whitespace sensitive**, but indentation is **not** important. The language
+is also case sensitive.
+
+**Warning**: There is no error handling yet, so if you make a mistake, the program will panic, or the result will be incorrect.
+
+## Usage
+The easiest way to use the language is to use the [`Interpreter`] struct. This will interpret the
+language and return the result. This is the default compiler for the language. In the future, there
+will be more compilers, such as an LLVM compiler or machine code compiler.
+
+## Source Files
+Source files are just text files with the `.laspa` extension. You can use any extension you want,
+but `.laspa` is the default. You can use the [`Interpreter::from_file`] method to read from a file.
+
+```rust
+use laspa::{Interpreter, Compile};
+
+let result = Interpreter::from_source("return + 1 2;");
+assert_eq!(result, 3.0);
+```
+ */
 
 use lazy_static::lazy_static;
 use regex::{Regex, Split};
 use std::{collections::HashMap, str::SplitWhitespace};
 
+/// The default number type. Every number is a [`f64`] number for simplicity.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Number(pub f64);
 
 impl Number {
+    /// Create a new number from a string. This will return an error if the string is not a valid
     pub fn new(s: &str) -> Result<Self, String> {
         match s.parse::<f64>() {
             Ok(n) => Ok(Self(n)),
@@ -16,17 +65,21 @@ impl Number {
     }
 }
 
+/// The default operator type. This is used for arithmetic and comparison operations.
 #[derive(Debug, PartialEq, Clone)]
 pub enum Op {
     Add,
     Sub,
     Mul,
     Div,
-    Gt, // Greater than
-    Lt, // Less than
+    /// Greater than
+    Gt,
+    /// Less than
+    Lt,
 }
 
 impl Op {
+    /// Create a new operator from a string. This will panic if the string is not a valid operator.
     pub fn new(s: &str) -> Self {
         match s {
             "+" => Self::Add,
@@ -40,6 +93,7 @@ impl Op {
     }
 }
 
+/// The default binary expression type. This is used for arithmetic and comparison operations (e.g. `+ 1 2` would equal `3`).
 #[derive(Debug, PartialEq, Clone)]
 pub struct BinaryExpr {
     pub op: Op,
@@ -47,29 +101,36 @@ pub struct BinaryExpr {
     pub rhs: Vec<Node>,
 }
 
+/// The default bind expression type. This is used to bind a value to a variable (e.g. `let x 10` binding the number `10` to `x`).
 #[derive(Debug, PartialEq, Clone)]
 pub struct BindExpr {
     pub name: String,
     pub value: Vec<Node>,
 }
 
+/// The default return expression type. This is used to return a value from a function. If this is not used, the last value in the function will be returned.
 #[derive(Debug, PartialEq, Clone)]
 pub struct ReturnExpr {
     pub value: Vec<Node>,
 }
 
+/// The default mutate expression type. This is used to mutate a variable (e.g. `:= x 10` setting the value of `x` to `10`).
+/// Variables can only be mutable.
 #[derive(Debug, PartialEq, Clone)]
 pub struct MutateExpr {
     pub name: String,
     pub value: Vec<Node>,
 }
 
+/// The default while expression type. This is used to create a while loop (e.g. `while < x 10` will loop while `x` is less than `10`).
 #[derive(Debug, PartialEq, Clone)]
 pub struct WhileExpr {
     pub condition: Vec<Node>,
     pub body: Vec<Node>,
 }
 
+/// The default if expression type. This is used to create an if statement (e.g. `if < x 10` will run the code in the if statement if `x` is less than `10`).
+/// The else statement is optional.
 #[derive(Debug, PartialEq, Clone)]
 pub struct IfExpr {
     pub condition: Vec<Node>,
@@ -77,6 +138,7 @@ pub struct IfExpr {
     pub else_body: Vec<Node>,
 }
 
+/// The default node type. This is used to represent every element of the language. This is used to create an abstract syntax tree (AST).
 #[derive(Debug, PartialEq, Clone)]
 pub enum Node {
     Number(Number),
@@ -93,10 +155,12 @@ lazy_static! {
     static ref RE: Regex = Regex::new(r"[;\n]").unwrap();
 }
 
+/// Lex a string into tokens. This will split the string into tokens, which can then be parsed into an AST.
 pub fn lex(s: &str) -> regex::Split<'static, '_> {
     RE.split(s)
 }
 
+/// Parse tokens into an AST. This will parse a string of tokens into an AST, which can then be evaluated.
 pub fn parse(tokens: &mut Split<'static, '_>) -> Vec<Node> {
     let mut nodes = Vec::new();
     while let Some(token) = tokens.next() {
@@ -128,6 +192,8 @@ pub fn parse(tokens: &mut Split<'static, '_>) -> Vec<Node> {
     nodes
 }
 
+/// Parse a sentence into an AST. This will parse a sentence into an AST, which can then be evaluated.
+/// Sentences are separated by newlines or `;` as provided by the regex in the lexer.
 fn parse_sentence(tokens: &mut SplitWhitespace) -> Result<Vec<Node>, String> {
     let mut nodes = Vec::new();
     match tokens.next() {
@@ -178,7 +244,11 @@ fn parse_sentence(tokens: &mut SplitWhitespace) -> Result<Vec<Node>, String> {
                 let condition = parse_sentence(tokens).unwrap();
                 let body = Vec::new();
                 let else_body = Vec::new();
-                nodes.push(Node::IfExpr(IfExpr { condition, body, else_body }));
+                nodes.push(Node::IfExpr(IfExpr {
+                    condition,
+                    body,
+                    else_body,
+                }));
             }
 
             _ => match Number::new(t) {
@@ -193,6 +263,8 @@ fn parse_sentence(tokens: &mut SplitWhitespace) -> Result<Vec<Node>, String> {
     Ok(nodes)
 }
 
+/// Evaluate an AST. This will evaluate an AST and return the result. All variables are in the global scope.
+/// This is essentially the interpreter for the language.
 pub fn eval(ast: &Vec<Node>, globals: &mut HashMap<String, f64>) -> f64 {
     let mut return_val: Option<f64> = None;
     let mut last_val: f64 = 0.0;
@@ -254,11 +326,16 @@ pub fn eval(ast: &Vec<Node>, globals: &mut HashMap<String, f64>) -> f64 {
     return_val.unwrap_or(last_val)
 }
 
+/// The default trait for compiling a language. This is used to compile a language from a specific source.
+/// This trait can be implemented for any output: llvm, interpreter, etc.
 pub trait Compile {
+    /// The output type of the compiler. Varies depending on the compiler.
     type Output;
 
+    /// Compile an AST into the output type.
     fn from_ast(nodes: Vec<Node>) -> Self::Output;
 
+    /// Compile a string into the output type.
     fn from_source(source: &str) -> Self::Output {
         let mut tokens = lex(source);
         // println!("tokens: {:?}", lex(source).collect::<Vec<_>>());
@@ -267,12 +344,14 @@ pub trait Compile {
         Self::from_ast(nodes)
     }
 
+    /// Compile a file into the output type. Supply the crate-relative path to the file.
     fn from_file(path: &str) -> Self::Output {
         let source = std::fs::read_to_string(path).unwrap();
         Self::from_source(&source)
     }
 }
 
+/// The default interpreter.
 pub struct Interpreter;
 
 impl Compile for Interpreter {
@@ -448,7 +527,6 @@ mod tests {
                 let y 2
                 
                 if < x y
-                then
                     return y
                 end
                 
